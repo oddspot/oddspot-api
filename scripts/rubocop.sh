524328a40c54e1041fdd7c77c9072c2766b2d157
Pavle Milicevic <pavle.milicevic@netbeez.net>
2019-12-30 23:56:52 -0500

    fix: Don’t lint if there is no changes.

--- scripts/rubocop.sh
+++ scripts/rubocop.sh
@@ -2,4 +2,10 @@
 
 FILES=$(git diff --name-only --diff-filter=MA origin/development..$CIRCLE_SHA1  | grep -E '\.(rb|rake)')
 echo $FILES
-# bundle exec rubocop $FILES
+
+if test -z "$FILES" 
+then
+      echo "There is no files to lint."
+else
+      bundle exec rubocop $FILES
+fi
